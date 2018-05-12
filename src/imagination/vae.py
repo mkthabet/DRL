c69f73d0400f9664d6cbd8dcb5e1390527ea40d8
mkthabet <mkthabetg@hotmail.com>
2018-05-12 20:17:03 +0100

    Great results for vae. tester now diplays reconstruction for a laten space manifold.

--- src/imagination/vae.py
+++ src/imagination/vae.py
@@ -1,8 +1,4 @@
-'''This script demonstrates how to build a variational autoencoder with Keras.
- #Reference
- - Auto-Encoding Variational Bayes
-   https://arxiv.org/abs/1312.6114
-'''
+
 from __future__ import print_function
 
 import numpy as np
@@ -20,11 +16,11 @@ from keras import metrics
 from keras.datasets import mnist
 
 batch_size = 30
-latent_dim = 3
+latent_dim = 4
 intermediate_dim = 8
-epochs = 50
+epochs = 2000
 epsilon_std = 1.0
-
+BETA = 4
 IMAGE_WIDTH =64
 IMAGE_HEIGHT = 64
 CHANNELS = 3
@@ -32,18 +28,10 @@ original_dim = IMAGE_HEIGHT*IMAGE_WIDTH*CHANNELS
 
 
 def processImage( img ):
-    #rgb = None
     image = cv2.resize(img, (IMAGE_WIDTH, IMAGE_HEIGHT))
     image = image.astype(float)/255
-    #print(np.max(image))
-    #image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #i = plt.imshow(image)
-    #plt.show()
     return image
-    #r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
-    #gray = 0.2989 * r + 0.5870 * g + 0.1140 * b     # extract luminance
-    #o = gray.astype('float32') / 128 - 1    # normalize
-    #return o
+
 
 def getImages():
     imgs= []
@@ -76,12 +64,10 @@ def getImages():
 def vae_loss(x, x_decoded_mean):
     x= K.batch_flatten(x)
     x_decoded_mean = K.batch_flatten(x_decoded_mean)
-    xent_loss = metrics.binary_crossentropy(x, x_decoded_mean)
-    #print ('shape = ', K.shape(metrics.binary_crossentropy(x, x_decoded_mean)))
+    #xent_loss = original_dim * metrics.binary_crossentropy(x, x_decoded_mean)
+    xent_loss =  original_dim* metrics.binary_crossentropy(x, x_decoded_mean)
     kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
-    #print('shape = ', kl_loss)
-    return K.mean(xent_loss + kl_loss)
-    #return K.mean((xent_loss)+ kl_loss)
+    return xent_loss + BETA*kl_loss
 
 
 img_input = Input(shape=(IMAGE_WIDTH, IMAGE_HEIGHT, CHANNELS))
@@ -122,36 +108,12 @@ decoder.summary()
 reconstructed = decoder(z)
 vae = Model(img_input, reconstructed, name='vae')
 opt = RMSprop(lr=0.00025)
-vae.compile(optimizer=opt, loss=vae_loss)
+vae.compile(optimizer='adam', loss=vae_loss)
 vae.summary()
 
-# we instantiate these layers separately so as to reuse them later
-
-#decoder_h = Dense(intermediate_dim, activation='relu')
-#decoder_mean = Dense(original_dim, activation='sigmoid')
-#h_decoded = decoder_h(z)
-#x_decoded_mean = decoder_mean(h_decoded)
-
-# instantiate VAE model
-#vae = Model(x, x_decoded_mean)
-
-# Compute VAE loss
-#xent_loss = original_dim * metrics.binary_crossentropy(x, x_decoded_mean)
-#kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
-#vae_loss = K.mean(xent_loss + kl_loss)
-
-#vae.compile(loss= vae_loss, optimizer='rmsprop')
-#vae.summary()
-
-
-# train the VAE on MNIST digits
-#(x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = y_train = x_test = y_test = getImages()
 
-#x_train = x_train.astype('float32') / 255.
-#x_test = x_test.astype('float32') / 255.
-#x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-#x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
 try:
     vae.fit(x_train, x_train,
             shuffle=True,
@@ -159,32 +121,13 @@ try:
             batch_size=batch_size,
             validation_data=(x_test, x_test))
 finally:
-    encoder.save('models/encoder_01.h5')
-    decoder.save('models/decoder_01.h5')
-    vae.save('models/vae_01.h5')
+    encoder.save('models/encoder_11.h5')
+    decoder.save('models/decoder_11.h5')
+    vae.save('models/vae_11.h5')
 
-# build a model to project inputs on the latent space
-#encoder = Model(x, z_mean)
-
-# display a 2D plot of the digit classes in the latent space
-#x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
-#plt.figure(figsize=(6, 6))
-#plt.scatter(x_test_encoded[:, 0], x_test_encoded[:, 1], c=y_test)
-#plt.colorbar()
-#plt.show()
-
-# build a digit generator that can sample from the learned distribution
-#decoder_input = Input(shape=(latent_dim,))
-#_h_decoded = decoder_h(decoder_input)
-#_x_decoded_mean = decoder_mean(_h_decoded)
-#generator = Model(decoder_input, _x_decoded_mean)
-
-# display a 2D manifold of the digits
-n = 15  # figure with 15x15 digits
+n = 15
 digit_size = 64
 figure = np.zeros((digit_size * n, digit_size * n, 3))
-# linearly spaced coordinates on the unit square were transformed through the inverse CDF (ppf) of the Gaussian
-# to produce values of the latent variables z, since the prior of the latent space is Gaussian
 grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
 grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
 
@@ -197,6 +140,6 @@ for i, yi in enumerate(grid_x):
         figure[i * digit_size: (i + 1) * digit_size,
                j * digit_size: (j + 1) * digit_size, :] = digit
 
-plt.figure(figsize=(10, 10))
-plt.imshow(figure, cmap='Greys_r')
+plt.figure()
+plt.imshow(figure)
 plt.show()
